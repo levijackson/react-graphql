515cfe28a37d892224f3fe5fa6557367f322c333
Levi Jackson <aerosox@gmail.com>
2021-07-04 14:17:48 -0400

    remove cred from repo after file name change

--- src/config/db.ts
+++
@@ -1,9 +0,0 @@
-const github = {
-    baseUrl: "https://api.github.com/graphql",
-    headers: {
-        "Content-Type": "application/json",
-        Authorization: "bearer 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"
-    }
-}
-
-export default github;
